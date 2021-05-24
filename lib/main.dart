028a2bae6a4e8d3bd428db48c19092758e865365
PauloAlbes8039 <pj38alves@gmail.com>
2021-05-24 09:49:17 -0300

    Adicionando projeto no repositório

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:aprenda_ingles/telas/Home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
